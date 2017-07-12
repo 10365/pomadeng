bd1ededa044b79374dd4edc21c5a7d74a92c3d0b
mac <mac@macdeMacBook-Pro.local>
2017-07-12 15:59:30 +0800

    commit

--- paoMaDeng/ViewController.m
+++ paoMaDeng/ViewController.m
@@ -9,17 +9,41 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UILabel *label;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title=@"跑马灯";
+    self.view.backgroundColor=[UIColor whiteColor];
+    
+    [self setUI];
 }
 
+-(void)setUI{
+    label=[[UILabel alloc] initWithFrame:CGRectMake(0, 100, 150, 20)];
+    label.text=@"跑马灯好看";
+    [self.view addSubview:label];
+    
+    CGRect frame = label.frame;
+    frame.origin.x = -150;
+    label.frame = frame;
+    [UIView beginAnimations:@"animation"context:NULL];
+    [UIView setAnimationDuration:5.0f];
+    [UIView setAnimationCurve:UIViewAnimationCurveLinear];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationRepeatAutoreverses:NO];
+    [UIView setAnimationRepeatCount:200];
+    frame = label.frame;
+    frame.origin.x =350;
+    label.frame = frame;
+    [UIView commitAnimations];
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
