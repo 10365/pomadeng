8c411d090a9217fb473ea3e2ce19ae1b15d3e3fa
ying <mac@localhost>
2017-07-12 15:27:39 +0800

    Initial Commit

---
+++ paoMaDeng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  paoMaDeng
+//
+//  Created by mac on 17/7/12.
+//  Copyright © 2017年 liying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ paoMaDeng/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  paoMaDeng
+//
+//  Created by mac on 17/7/12.
+//  Copyright © 2017年 liying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
